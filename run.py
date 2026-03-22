44e08b20b038f31f4430a7cc28d98abb873c8180
Franq-TechMind <techmindhub1@gmail.com>
2026-03-23 01:35:52 +0300

    Switch to PostgreSQL: add psycopg2-binary and production config

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
-app = create_app()
+config_class = 'config.ProductionConfig' if os.environ.get('FLASK_ENV') == 'production' else 'config.DevelopmentConfig'
+app = create_app(config_class)
 
 if __name__ == '__main__':
     app.run(debug=True)
